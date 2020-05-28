bfa11a81be46cc245ed4a1a98f40e3874c6379fb
Lakulish Antani <lakulisha@valvesoftware.com>
2020-05-28 11:57:05 -0700

    Add Linux build scripts

---
+++ build/linux-x86/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cmake \
+    -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS=-m32 \
+    -DCMAKE_SHARED_LINKER_FLAGS=-m32 \
+    -DBUILD_TESTING=FALSE \
+    -DEMBREE_ISPC_SUPPORT=TRUE \
+    -DEMBREE_STATIC_LIB=TRUE \
+    -DEMBREE_STATIC_RUNTIME=TRUE \
+    -DEMBREE_TASKING_SYSTEM=INTERNAL \
+    -DEMBREE_TUTORIALS=FALSE \
+    ../..
+
+cmake --build .
